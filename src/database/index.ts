7911cf28f821a6f78b2dda30725118a2af1add12
Henning <henning@inspia.de>
2020-02-24 18:37:08 +0100

    Update database startup

--- src/database/index.ts
+++ src/database/index.ts
@@ -9,7 +9,7 @@ export async function connect(databaseFN: string) {
     return (_connection = await createConnection({
         type: 'sqlite',
         database: databaseFN,
-        entities: ['entity/*.ts', 'entity/*.js'],
+        entities: [ User, Offer, PostCode ],
         logging: true,
     }));
 }
