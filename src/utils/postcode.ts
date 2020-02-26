95a1ae3b279a13477aab41220a880b01a458daad
Henning <=henning@inspia.de>
2020-02-26 22:16:58 +0100

    Remove console log

--- src/utils/postcode.ts
+++ src/utils/postcode.ts
@@ -1,5 +1,4 @@
 export function validatePostcode(postcode: string): boolean {
-    console.log(postcode);
     const regExp = new RegExp(/^(?!01000|99999)(0[1-9]\d{3}|[1-9]\d{4})$/);
     return regExp.test(postcode);
 }
