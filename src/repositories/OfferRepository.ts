59dea07138b15edba411a3934f0230f730f4e8dc
Henning <=henning@inspia.de>
2020-02-28 08:59:08 +0100

    Remove unused repositories

--- src/repositories/OfferRepository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Offer } from '../entity/Offer';
-
-@EntityRepository(Offer)
-export class OfferRepository extends Repository<Offer> {}
--- src/repositories/PostCodeRepository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { PostCode } from '../entity/PostCode';
-
-@EntityRepository(PostCode)
-export class PostCodeRepository extends Repository<PostCode> {}
