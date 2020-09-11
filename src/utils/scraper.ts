91f6172d19bedd1b6a73570d3926fb6217757e3c
Quarko <henning@inspia.de>
2020-09-11 22:14:40 +0200

    Fix bug to handle multiple entries

--- src/utils/scraper.ts
+++ src/utils/scraper.ts
@@ -66,96 +66,107 @@ export class FlaschenpostScraper {
             await page.goto(`${this.baseUrl}/bier/${category}`);
             await page.waitForSelector('#fp-productList', { timeout: this.timeout });
 
-            const offers = await page.evaluate(category => {
-                const elements: any = [...document.getElementsByClassName('fp-productList_highlight')];
-                const tmp = [];
+            const getOffers = async () => {
+                return await page.evaluate(async (category) => {
+                    const elements: any = [...document.getElementsByClassName('fp-productList_highlight')];
+                    const tmp = [];
 
-                elements
-                    .filter(e => {
-                        if (e.innerText === 'TOP-ANGEBOT') {
-                            return e;
-                        }
+                    elements
+                        .filter(e => {
+                            if (e.innerText === 'TOP-ANGEBOT') {
+                                return e;
+                            }
+                        })
+                        .map(e => {
+                            console.log("e: ", e);
+                            //for(const e of elements) {
+                            if (e.innerText === 'TOP-ANGEBOT') {
+                                const element = e.parentElement;
+                                const name = element
+                                    .getElementsByClassName('fp-productList_productName')[0]
+                                    .textContent.split('\n')[1]
+                                    .trim();
+
+                                const priceOffer = element.getElementsByClassName('fp-productList_detail')[0];
+
+                                const bottleDetails = priceOffer
+                                    .getElementsByClassName('fp-productList_bottleInfo')[0]
+                                    .innerText;
+
+                                const offer = {
+                                    name: name,
+                                    category: category,
+                                    bottleAmount: 0,
+                                    bottleSize: 0,
+                                    oldPrice: 0.0,
+                                    price: 0.0,
+                                };
+
+                                if (bottleDetails.length >= 2) {
+                                    const split = bottleDetails.split(' ');
+                                    offer.bottleAmount = parseInt(split[split.length - 4]);
+                                    offer.bottleSize = parseFloat(
+                                        split[split.length - 2]
+                                            .replace('L', '')
+                                            .replace(',', '.'),
+                                    );
+                                }
+
+                                if (priceOffer.getElementsByClassName('fp-productList_price--stroke').length > 0) {
+
+                                    offer.oldPrice = parseFloat(
+                                        priceOffer
+                                            .getElementsByClassName('fp-productList_price--stroke')[0]
+                                            .innerText.split(' ')[0]
+                                            .replace(',', '.'),
+                                    );
+                                }
+                                else {
+                                    return;
+                                }
+
+                                if (priceOffer.getElementsByClassName('fp-price-is-special-offer').length > 0) {
+
+                                    offer.price = parseFloat(
+                                        priceOffer
+                                            .getElementsByClassName('fp-price-is-special-offer')[0]
+                                            .innerText.split(' ')[0]
+                                            .replace(',', '.'),
+                                    );
+                                }
+                                else {
+                                    return;
+                                }
+
+                                if (!tmp.some(x => JSON.stringify(x) === JSON.stringify(offer))) {
+                                    tmp.push(offer);
+                                }
+                            }
+                        });
+                    return await new Promise(resolve => {
+                        resolve(tmp);
                     })
-                    .map(e => {
-                        //for(const e of elements) {
-                        if (e.innerText === 'TOP-ANGEBOT') {
-                            const element = e.parentElement;
-                            const name = element
-                                .getElementsByClassName('fp-productList_productName')[0]
-                                .textContent.split('\n')[1]
-                                .trim();
+                }, 'pils');
+            }
+            return await getOffers();
+            // .map(offer => {
+            //     const tmp = new Offer();
 
-                            const priceOffer = element.getElementsByClassName('fp-productList_detail')[0];
+            //     tmp.name = offer.name;
+            //     tmp.category = offer.category;
+            //     tmp.bottleAmount = offer.bottleAmount;
+            //     tmp.bottleSize = offer.bottleSize;
+            //     tmp.oldPrice = offer.oldPrice;
+            //     tmp.price = offer.price;
 
-                            const bottleDetails = priceOffer
-                                .getElementsByClassName('fp-productList_bottleDetails')[0]
-                                .innerText.split('\n')[0];
-
-                            const offer = {
-                                name: name,
-                                category: category,
-                                bottleAmount: 0,
-                                bottleSize: 0,
-                                oldPrice: 0.0,
-                                price: 0.0,
-                            };
-
-                            if (bottleDetails.length >= 2) {
-                                offer.bottleAmount = parseInt(bottleDetails.split(' ')[bottleDetails.length - 2]);
-                                offer.bottleSize = parseFloat(
-                                    bottleDetails
-                                        .split(' ')[bottleDetails.length - 1]
-                                        .replace('L', '')
-                                        .replace(',', '.'),
-                                );
-                            }
-
-                            if (priceOffer.getElementsByClassName('fp-productList_price--stroke').length > 0) {
-                                offer.oldPrice = parseFloat(
-                                    priceOffer
-                                        .getElementsByClassName('fp-productList_price--stroke')[0]
-                                        .innerText.split(' ')[0]
-                                        .replace(',', '.'),
-                                );
-                            } else {
-                                return;
-                            }
-
-                            if (priceOffer.getElementsByClassName('fp-price-is-special-offer').length > 0) {
-                                offer.price = parseFloat(
-                                    priceOffer
-                                        .getElementsByClassName('fp-price-is-special-offer')[0]
-                                        .innerText.split(' ')[0]
-                                        .replace(',', '.'),
-                                );
-                            } else {
-                                return;
-                            }
-
-                            if (!tmp.some(x => JSON.stringify(x) === JSON.stringify(offer))) {
-                                tmp.push(offer);
-                            }
-                        }
-                    });
-                return tmp;
-            }, 'pils');
-            await browser.close();
-            return offers.map(offer => {
-                const tmp = new Offer();
-
-                tmp.name = offer.name;
-                tmp.category = offer.category;
-                tmp.bottleAmount = offer.bottleAmount;
-                tmp.bottleSize = offer.bottleSize;
-                tmp.oldPrice = offer.oldPrice;
-                tmp.price = offer.price;
-
-                return tmp;
-            });
+            //     return tmp;
+            // });
         } catch (err) {
             await browser.close();
 
             return err;
+        } finally {
+            await browser.close();
         }
     }
 }
