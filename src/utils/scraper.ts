a334372ccb69eebd3fad1223a8ff0478619bd1e0
Quarko <henning@inspia.de>
2020-09-11 23:09:22 +0200

    Add additional categories

--- src/utils/scraper.ts
+++ src/utils/scraper.ts
@@ -10,16 +10,16 @@ export class FlaschenpostScraper {
     private categories: string[] = [
         'pils',
         'helles',
-        'export',
-        'weissbier',
+        // 'export',
+        'weizen-weissbier',
         'alkoholfrei',
-        'radler',
-        'biermischgetraenke',
+        // 'radler',
+        // 'biermischgetraenke',
         'altbier',
-        'malzbier',
-        'land-kellerbier',
-        'spezialitaeten',
-        'sonstige',
+        // 'malzbier',
+        // 'land-kellerbier',
+        // 'spezialitaeten',
+        // 'sonstige',
     ];
 
     constructor(baseUrl: string) {
@@ -48,6 +48,88 @@ export class FlaschenpostScraper {
         }
     }
 
+    private async getOffers(category: string, page) {
+        return await page.evaluate(async (category) => {
+            const elements: any = [...document.getElementsByClassName('fp-productList_highlight')];
+            const tmp = [];
+
+            elements
+                .filter(e => {
+                    if (e.innerText === 'TOP-ANGEBOT') {
+                        return e;
+                    }
+                })
+                .map(e => {
+                    //for(const e of elements) {
+                    if (e.innerText === 'TOP-ANGEBOT') {
+                        const element = e.parentElement;
+                        const name = element
+                            .getElementsByClassName('fp-productList_productName')[0]
+                            .textContent.split('\n')[1]
+                            .trim();
+
+                        const priceOffer = element.getElementsByClassName('fp-productList_detail')[0];
+
+                        const bottleDetails = priceOffer
+                            .getElementsByClassName('fp-productList_bottleInfo')[0]
+                            .innerText;
+
+                        const offer = {
+                            name: name,
+                            category: category,
+                            bottleAmount: 0,
+                            bottleSize: 0,
+                            oldPrice: 0.0,
+                            price: 0.0,
+                        };
+
+                        if (bottleDetails.length >= 2) {
+                            const split = bottleDetails.split(' ');
+                            offer.bottleAmount = parseInt(split[split.length - 4]);
+                            offer.bottleSize = parseFloat(
+                                split[split.length - 2]
+                                    .replace('L', '')
+                                    .replace(',', '.'),
+                            );
+                        }
+
+                        if (priceOffer.getElementsByClassName('fp-productList_price--stroke').length > 0) {
+
+                            offer.oldPrice = parseFloat(
+                                priceOffer
+                                    .getElementsByClassName('fp-productList_price--stroke')[0]
+                                    .innerText.split(' ')[0]
+                                    .replace(',', '.'),
+                            );
+                        }
+                        else {
+                            return;
+                        }
+
+                        if (priceOffer.getElementsByClassName('fp-price-is-special-offer').length > 0) {
+
+                            offer.price = parseFloat(
+                                priceOffer
+                                    .getElementsByClassName('fp-price-is-special-offer')[0]
+                                    .innerText.split(' ')[0]
+                                    .replace(',', '.'),
+                            );
+                        }
+                        else {
+                            return;
+                        }
+
+                        if (!tmp.some(x => JSON.stringify(x) === JSON.stringify(offer))) {
+                            tmp.push(offer);
+                        }
+                    }
+                });
+            return await new Promise(resolve => {
+                resolve(tmp);
+            })
+        }, category);
+    }
+
     async runWithPC(pc: string): Promise<Offer[]> {
         const browser = await puppeteer.launch({
             args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
@@ -62,109 +144,42 @@ export class FlaschenpostScraper {
             await page.click('button.zip--button');
             await page.waitForSelector('.fp-modal_inner', { timeout: this.timeout, hidden: true });
 
-            const category = 'pils';
-            await page.goto(`${this.baseUrl}/bier/${category}`);
-            await page.waitForSelector('#fp-productList', { timeout: this.timeout });
+            let result = [];
 
-            const getOffers = async () => {
-                return await page.evaluate(async (category) => {
-                    const elements: any = [...document.getElementsByClassName('fp-productList_highlight')];
-                    const tmp = [];
+            for (const category of this.categories) {
+                console.log(`Checking offers for ${pc}: ${category}`);
 
-                    elements
-                        .filter(e => {
-                            if (e.innerText === 'TOP-ANGEBOT') {
-                                return e;
-                            }
-                        })
-                        .map(e => {
-                            console.log("e: ", e);
-                            //for(const e of elements) {
-                            if (e.innerText === 'TOP-ANGEBOT') {
-                                const element = e.parentElement;
-                                const name = element
-                                    .getElementsByClassName('fp-productList_productName')[0]
-                                    .textContent.split('\n')[1]
-                                    .trim();
+                await page.goto(`${this.baseUrl}/bier/${category}`);
 
-                                const priceOffer = element.getElementsByClassName('fp-productList_detail')[0];
+                try {
+                    const failure = await page.$eval("h2", e => e.innerHTML);
+                    console.log("Fehler: ", failure);
+                    if (failure.trim() === "Fehler!") {
+                        continue;
+                    }
+                } catch(error) {
+                    console.log(`Skipping category ${category} because it does not exist at postcode ${pc}`);
+                }
 
-                                const bottleDetails = priceOffer
-                                    .getElementsByClassName('fp-productList_bottleInfo')[0]
-                                    .innerText;
+                await page.waitForSelector('#fp-productList', { timeout: this.timeout });
+                
+                let offers: Offer[];
+                try {
+                    offers = await this.getOffers(category, page);
+                } catch (error) {
+                    continue;
+                }
 
-                                const offer = {
-                                    name: name,
-                                    category: category,
-                                    bottleAmount: 0,
-                                    bottleSize: 0,
-                                    oldPrice: 0.0,
-                                    price: 0.0,
-                                };
+                console.log(`Found ${offers.length} results for ${pc}`);
 
-                                if (bottleDetails.length >= 2) {
-                                    const split = bottleDetails.split(' ');
-                                    offer.bottleAmount = parseInt(split[split.length - 4]);
-                                    offer.bottleSize = parseFloat(
-                                        split[split.length - 2]
-                                            .replace('L', '')
-                                            .replace(',', '.'),
-                                    );
-                                }
-
-                                if (priceOffer.getElementsByClassName('fp-productList_price--stroke').length > 0) {
-
-                                    offer.oldPrice = parseFloat(
-                                        priceOffer
-                                            .getElementsByClassName('fp-productList_price--stroke')[0]
-                                            .innerText.split(' ')[0]
-                                            .replace(',', '.'),
-                                    );
-                                }
-                                else {
-                                    return;
-                                }
-
-                                if (priceOffer.getElementsByClassName('fp-price-is-special-offer').length > 0) {
-
-                                    offer.price = parseFloat(
-                                        priceOffer
-                                            .getElementsByClassName('fp-price-is-special-offer')[0]
-                                            .innerText.split(' ')[0]
-                                            .replace(',', '.'),
-                                    );
-                                }
-                                else {
-                                    return;
-                                }
-
-                                if (!tmp.some(x => JSON.stringify(x) === JSON.stringify(offer))) {
-                                    tmp.push(offer);
-                                }
-                            }
-                        });
-                    return await new Promise(resolve => {
-                        resolve(tmp);
-                    })
-                }, 'pils');
+                result = [ ...result, ...offers];
             }
-            return await getOffers();
-            // .map(offer => {
-            //     const tmp = new Offer();
+            return result;
 
-            //     tmp.name = offer.name;
-            //     tmp.category = offer.category;
-            //     tmp.bottleAmount = offer.bottleAmount;
-            //     tmp.bottleSize = offer.bottleSize;
-            //     tmp.oldPrice = offer.oldPrice;
-            //     tmp.price = offer.price;
-
-            //     return tmp;
-            // });
         } catch (err) {
             await browser.close();
 
-            return err;
+            console.log("Scraper Error: ", err);
         } finally {
             await browser.close();
         }
